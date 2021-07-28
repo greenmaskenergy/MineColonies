0cada1bcc6bb6f4bdf4d659912688d6c551629e4
greenmaskenergy <greenmaskenergy@gmail.com>
2021-07-28 18:15:56 +0200

    fixed: not working with spaces in file path, some visual tweaks and neater log file layout

--- updateMods.bat
+++ updateMods.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 cls
 title MineColonies Mod Downloader
 echo by GreenMaskEnergy [90mversion 2.1[0m 
@@ -9,26 +9,24 @@ echo Will download only missing mods in the same directory as this is run from!
 echo place in (minecraft)/mods folder and run there
 echo =============================================================================
 echo.
-REM checking if parent folder is actualy called mods! to make sure it download to the right folder
-REM setting the variable to the parent folder name
 echo this downloader is located at:
 echo.
 set "RawPath=%~dp0"
-for %%I in ("%~dp0") do for %%J in ("%%~dpI.") do set Parent=%%~nxJ
+for %%I in ("%~dp0") do for %%J in ("%%~dpI.") do set "Parent=%%~nxJ"
 for /D %%D in ("%~dp0.") do (set "ColorRawPath=%%~dpD")
-
-
+set "log=%RawPath%/latestLog.txt"
 if %Parent% == mods (
     echo [36m%ColorRawPath%[0m[32m%Parent%[0m
     echo.
     echo.
     echo.
     pause>nul|set/p =[36mpress any key to start[0m downloading missing mods.
-    echo.Downloader started from %RawPath%> %RawPath%/latestLog.txt
-    echo.%date% %time% >> %RawPath%/latestLog.txt
+    echo --------------------------------------------------------------------------------------------- > "%log%"
+    echo.Location 📁 %RawPath% >> "%log%"
+    echo.Date     🕐 %date%>> "%log%"
     goto dowloadingMissingMods
 ) else (
-    echo [36m%RawPath%[0m[31m%Parent%[0m
+    echo [36m%ColorRawPath%[0m[31m%Parent%[0m
     echo.
     echo [91mWrong directory this downloader needs to be placed in "/mod" Folder[0m
     echo.
@@ -41,32 +39,82 @@ if %Parent% == mods (
 
 
 :dowloadingMissingMods
+set "modsfile=%RawPath%/mods"
 set /a progress=0
-echo Downloading ModListFile (mods) >> %RawPath%/latestLog.txt
-echo --------------------------------------------------------------------------------------------- >> %RawPath%/latestLog.txt
-echo.  TimeStamp   progress  status    modname >> %RawPath%/latestLog.txt
-echo --------------------------------------------------------------------------------------------- >> %RawPath%/latestLog.txt
-bitsadmin.exe /transfer "" /priority FOREGROUND https://onehit.eu/MC_ModLists/MineColonies/mods %~dp0/mods
-REM >> %RawPath%/latestLog.txt
-set "cmd=findstr /R /N "^^" %~dp0\mods | find /C ":""
+echo --------------------------------------------------------------------------------------------- >> "%log%"
+echo.  TimeStamp     Progress     Status      Modname >> "%log%"
+echo --------------------------------------------------------------------------------------------- >> "%log%"
+echo.[%time%]      /      ✅ Download    ModListFile (mods) >> "%log%"
+bitsadmin.exe /transfer "" /priority FOREGROUND https://onehit.eu/MC_ModLists/MineColonies/mods "%RawPath%/mods"
+echo. >> "%log%"
+set "cmd=findstr /R /N "^^" "%RawPath%\mods" | find /C ":""
 for /f %%a in ('!cmd!') do set ModCount=%%a
-
-for /F "tokens=*" %%A in (%~dp0\mods) do (
-    set /a progress=progress+1
-    REM echo Mod Progress: !progress! / %ModCount% >> %RawPath%/latestLog.txt
-
-if not exist %~dp0\%%A (
-    echo [%time%]  !progress! / %ModCount%   Install  %%A >> %RawPath%/latestLog.txt
-    bitsadmin.exe /transfer "Mod (!progress! / !ModCount!): %%A"  /priority FOREGROUND https://github.com/greenmaskenergy/MineColonies/raw/master/%%A %~dp0\%%A
-) else (
-    echo [%time%]  !progress! / %ModCount%   Skipped   %%A >> %RawPath%/latestLog.txt
+set "space=DEF"
+set "modsSkipped=0"
+set "modsDownloaded=0"
+FOR /F "USEBACKQ TOKENS=*" %%A IN ("%RawPath%/mods") DO (
+    set /a progress=!progress!+1
+    if !progress! LSS 1000 (
+        set "space="
+        if !progress! LSS 100 (
+            set "space= "
+            if !progress! LSS 10 (
+                set "space=  "
+            )
+        )
+    )
+    REM set spacer=" "*(%ModCount%)
+    if not exist "%~dp0\%%~A" (
+        set /a modsDownloaded=!modsDownloaded!+1
+        echo [%time%]  !space!!progress! / %ModCount%   ✅ Download    %%A >> "%log%"
+        bitsadmin.exe /transfer "Mod (!progress! / !ModCount!): %%A"  /priority FOREGROUND https://github.com/greenmaskenergy/MineColonies/raw/master/%%A "%RawPath%/%%A"
+    ) else (
+        set /a modsSkipped=!modsSkipped!+1
+        echo [%time%]  !space!!progress! / %ModCount%   ❌ Skipped     %%A >> "%log%"
+    )
 )
-)
-echo --------------------------------------------------------------------------------------------- >> %RawPath%/latestLog.txt
-echo update done! >> %RawPath%/latestLog.txt
-REM timeout /t 5 /nobreak >nul >> %RawPath%/latestLog.txt
+echo --------------------------------------------------------------------------------------------- >> "%log%"
+echo.Mods ✅ downloaded: !modsDownloaded! >> "%log%"
+echo.Mods ❌ skipped:    !modsSkipped! >> "%log%"
+cls
+REM pause>nul|set/p =[32mDownloader Done![0m press any key to close
+echo [92mDownloader Done![0m
 echo.
+echo [36mMods downloaded: !modsDownloaded! [0m
+echo [33mMods skipped: !modsSkipped! [0m
 echo.
-pause>nul|set/p =[32mDownloader Done![0m press any key to close
-echo closing downloader >> %RawPath%/latestLog.txt
+echo closing in 5s
+timeout 1 /nobreak >nul
+cls
+echo [92mDownloader Done![0m
+echo.
+echo [36mMods downloaded: !modsDownloaded! [0m
+echo [33mMods skipped: !modsSkipped! [0m
+echo.
+echo closing in 4s
+timeout 1 /nobreak >nul
+cls
+echo [92mDownloader Done![0m
+echo.
+echo [36mMods downloaded: !modsDownloaded! [0m
+echo [33mMods skipped: !modsSkipped! [0m
+echo.
+echo closing in 3s
+timeout 1 /nobreak >nul
+cls
+echo [92mDownloader Done![0m
+echo.
+echo [36mMods downloaded: !modsDownloaded! [0m
+echo [33mMods skipped: !modsSkipped! [0m
+echo.
+echo closing in 2s
+timeout 1 /nobreak >nul
+cls
+echo [92mDownloader Done![0m
+echo.
+echo [36mMods downloaded: !modsDownloaded! [0m
+echo [33mMods skipped: !modsSkipped! [0m
+echo.
+echo closing in 1s
+timeout 1 /nobreak >nul
 EXIT /B 0
